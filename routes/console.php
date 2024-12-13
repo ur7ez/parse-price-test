f5a322b3e9e1963306f9f42976d5c1bb7aa11c1f
ur7ez <ur7ez@ukr.net>
2024-12-13 23:50:12 +0200

    added method and comments in scheduled command

--- routes/console.php
+++ routes/console.php
@@ -7,13 +7,20 @@ use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+})->purpose('Display an inspiring quote')->everySixHours();
 
-Schedule::command(ParseOlxPrice::class)
+Schedule::command(ParseOlxPrice::class, ['--method=http'])
     ->hourly()
     ->withoutOverlapping()
     ->appendOutputTo(storage_path('logs/parse_olx_price.log'));
 
 // to test command run:
-// php artisan schedule:test --name=parse:olx-price
-// php artisan schedule:work
+//          php artisan schedule:test --name="parse:olx-price --method=http"
+
+// To run a scheduled worker locally, run
+//          php artisan schedule:work
+// This command will run in the foreground and invoke the scheduler every minute until you terminate the command.
+// To run scheduled tasks once (without worker):
+//          php artisan schedule:run
+// Ensure that your queue worker is also running (ParseOlxPrice command queues emails):
+//          php artisan queue:work
